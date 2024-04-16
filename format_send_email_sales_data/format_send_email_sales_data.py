7d4bac7fa2cd5c4309d17cce04a8117954c9dec8
Lucas Zordan <lucaszordanb@gmail.com>
2024-04-16 09:09:56 -0300

    Update format_send_email_sales_data.py

--- format_send_email_sales_data/format_send_email_sales_data.py
+++ format_send_email_sales_data/format_send_email_sales_data.py
@@ -1,83 +1,68 @@
-<h1><b> Passo a passo para resolver o problema </h1></b>
-Passo 0: Entender o problema e suas restrições<br/>
-Passo 1: Importar a base de dados<br/>
-Passo 2: Calcular o produto mais vendido (quantidade) <br/>
-Passo 3: Calcular o produto mais vendido (faturamento em reais) <br/>
-Passo 4: Calcular o loja que mais vendeu (faturamento em reais) <br/>
-Passo 5: Calcular o ticket médio por loja <br/>
-Passo 6: Criar um dasboard da loja que mais vendeu (faturamento reais) <br/>
-Passo 7: Enviar um email automátiico <br/>
-
-<i> Blibiotecas exploradas: Pandas, Plotly, Smtplib, Babel.Numbers </i>
-
-#CRIAÇÃO DOS INDICADORES
-"""
-
-#Importação das bibliotecas
+#importing libraries
 
 import pandas as pd
 import plotly.express as px
 import smtplib
 import email.message as em
 
-#Passo 1: Importar a base de dados
+#step 1: import database
 
-tabela_vendas=pd.read_excel("/content/drive/MyDrive/Udemy_PythonDS/Project 1/Vendas.xlsx")
-tabela_vendas = tabela_vendas.rename(columns={'Valor Unitário':'Valor_Unitario','Cód.':'Cod'})
+df_sales=pd.read_excel("/content/drive/MyDrive/Udemy_PythonDS/Project 1/Sales.xlsx")
 
-# Passo 2: Calcular o produto mais vendido (quantidade)
+#step 2: Calculate the best-selling product (quantity)
 
-tb_quantidade_produto= tabela_vendas.groupby('Produto').sum() #por conta da função groupby a coluna que faz o agrupamento vira index- indexador unico
-tb_quantidade_produto= tb_quantidade_produto[['Quantidade']]
-tb_quantidade_produto = tb_quantidade_produto.sort_values(by='Quantidade', ascending=False)
+df_quantity_product= df_sales.groupby('Product').sum() #Due to the groupby function, the column that does the grouping becomes an index - unique indexer
+df_quantity_product= df_quantity_product[['Quantity']]
+df_quantity_product = df_quantity_product.sort_values(by='Quantity', ascending=False)
 
-#Passo 3: Calcular o produto mais vendido (faturamento em reais)
+#step 3: Calculate the best-selling product (profit)
 
-tabela_vendas['Faturamento_Reais'] = tabela_vendas['Quantidade']*tabela_vendas['Valor_Unitario']
-tb_faturamento_produto = tabela_vendas.groupby('Produto').sum()
-tb_faturamento_produto = tb_faturamento_produto[['Faturamento_Reais']]
-tb_faturamento_produto = tb_faturamento_produto.sort_values(by='Faturamento_Reais', ascending=False)
+df_sales['Profit'] = df_sales['Quantity']*df_sales['Unitary_value']
+df_profit_product = df_sales.groupby('Product').sum()
+df_profit_product = df_profit_product[['Profit']]
+df_profit_product = df_profit_product.sort_values(by='Profit', ascending=False)
 
-# Passo 4: Calcular o loja que mais vendeu (faturamento em reais)
-tb_faturamento_loja = tabela_vendas.groupby('Loja').sum()
-tb_faturamento_loja = tb_faturamento_loja[['Faturamento_Reais']]
-tb_faturamento_loja = tb_faturamento_loja.sort_values(by='Faturamento_Reais', ascending=False)
+# Passo 4: Calculate the store that sold the most (profit)
 
-# Passo 5: Calcular o ticket médio por loja]
+df_profit_store = df_sales.groupby('Store').sum()
+df_profit_store = df_profit_store[['Profit']]
+df_profit_store = df_profit_store.sort_values(by='Profit', ascending=False)
 
-tabela_vendas['Ticket_Medio'] = tabela_vendas['Valor_Unitario']
-tb_ticket_loja = tabela_vendas.groupby('Loja').mean(numeric_only=True)
-tb_ticket_loja = tb_ticket_loja[['Ticket_Medio']]
-tb_ticket_loja = tb_ticket_loja.sort_values(by='Ticket_Medio', ascending=False)
+# Passo 5: Calculate the average ticket per store
 
-# Passo 6: Criar um dasboard da loja que mais vendeu (faturamento reais)
+df_sales['Average_ticket'] = df_sales['Unitary_value']
+df_ticket_store = df_sales.groupby('Store').mean(numeric_only=True)
+df_ticket_store = df_ticket_store[['Average_ticket']]
+df_ticket_store = df_ticket_store.sort_values(by='Average_ticket', ascending=False)
 
-grafico = px.bar(tb_faturamento_loja, y='Faturamento_Reais', x=tb_faturamento_loja.index) #por conta da função groupby a coluna loja virou index
+# Passo 6: Create a dashboard of the store that sold the most (profit)
 
-"""#FORMATAÇÃO NUMÉRICA E MONETÁRIA"""
+graph = px.bar(df_profit_store, y='Profit', x=df_profit_store.index) #Due to the groupby function, the store column became index
+
+#NUMERICAL AND MONETARY FORMATTING
 
 from babel.numbers import format_currency
-#As formatações devem ser feitas separadamente e após os cálculos, em células separadas, porque fazem com que os valores passem a ser texto
+#Formatting must be done separately and after calculations, in separated cells, because they make the values become text
 
-#Faturamento por loja
-tb_faturamento_loja_formatado = pd.DataFrame(tb_faturamento_loja['Faturamento_Reais'].apply(lambda x: format_currency(x,'BRL',locale='pt_BR'))) #formatar moeda
-tb_faturamento_loja_formatado = tb_faturamento_loja_formatado.reset_index() #organizar cabeçalho, resetando index
-tb_faturamento_loja_formatado_html = tb_faturamento_loja_formatado.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">') #se torna uma estrutura html e não df pandas
+#Faturamento por Store
+df_profit_store_formatted = pd.DataFrame(df_profit_store['Profit'].apply(lambda x: format_currency(x,'BRL',locale='pt_BR'))) #formatar moeda
+df_profit_store_formatted = df_profit_store_formatted.reset_index() #organizar cabeçalho, resetando index
+df_profit_store_formatted_html = df_profit_store_formatted.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">') #se torna uma estrutura html e não df pandas
 
 #Quantidade vendida por produto
-tb_quantidade_produto_formatado = pd.DataFrame(tb_quantidade_produto['Quantidade'].apply(lambda x: '{:,.2f}'.format(x).replace(',','.'))) #formatar numero
-tb_quantidade_produto_formatado = tb_quantidade_produto_formatado.reset_index()
-tb_quantidade_produto_formatado_html = tb_quantidade_produto_formatado.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">')
+df_quantity_product_formatted = pd.DataFrame(df_quantity_product['Quantity'].apply(lambda x: '{:,.2f}'.format(x).replace(',','.'))) #formatar numero
+df_quantity_product_formatted = df_quantity_product_formatted.reset_index()
+df_quantity_product_formatted_html = df_quantity_product_formatted.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">')
 
 #Faturamento por produto
-tb_faturamento_produto_formatado = pd.DataFrame(tb_faturamento_produto['Faturamento_Reais'].apply(lambda x: format_currency(x,'BRL',locale='pt_BR')))
-tb_faturamento_produto_formatado = tb_faturamento_produto_formatado.reset_index()
-tb_faturamento_produto_formatado_html = tb_faturamento_produto_formatado.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">')
+df_profit_product_formatted = pd.DataFrame(df_profit_product['Profit'].apply(lambda x: format_currency(x,'BRL',locale='pt_BR')))
+df_profit_product_formatted = df_profit_product_formatted.reset_index()
+df_profit_product_formatted_html = df_profit_product_formatted.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">')
 
-#Ticket médio por loja
-tb_ticket_loja_formatado = pd.DataFrame(tb_ticket_loja['Ticket_Medio'].apply(lambda x: format_currency(x,'BRL',locale='pt_BR')))
-tb_ticket_loja_formatado = tb_ticket_loja_formatado.reset_index()
-tb_ticket_loja_formatado_html = tb_ticket_loja_formatado.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">')
+#Ticket médio por Store
+df_ticket_store_formatted = pd.DataFrame(df_ticket_store['Average_ticket'].apply(lambda x: format_currency(x,'BRL',locale='pt_BR')))
+df_ticket_store_formatted = df_ticket_store_formatted.reset_index()
+df_ticket_store_formatted_html = df_ticket_store_formatted.to_html(index=False,justify='center',border='0').replace('<tbody>', '<tbody style= "text-align: center; color: #484848; background: #F7F7F7">')
 
 """#ENVIAR EMAIL"""
 
@@ -91,17 +76,17 @@ corpo_email = f"""
 
 <p>Seguem indicadores de venda</p>
 
-<p><b>Faturamento por loja</p></b>
-<p>{tb_faturamento_loja_formatado_html}</p>
+<p><b>Faturamento por Store</p></b>
+<p>{df_profit_store_formatted_html}</p>
 
 <p><b>Quantidade vendida por produto</b></p>
-<p>{tb_quantidade_produto_formatado_html}</p>
+<p>{df_quantity_product_formatted_html}</p>
 
 <p><b>Faturamento por produto</p>
-<p>{tb_faturamento_produto_formatado_html} </b></p>
+<p>{df_profit_product_formatted_html} </b></p>
 
-<p><b>Ticket médio por loja</p></b>
-<p>{tb_ticket_loja_formatado_html} </p>
+<p><b>Ticket médio por Store</p></b>
+<p>{df_ticket_store_formatted_html} </p>
 
 <p>Qualquer duvida estou à disposição</p>
 <p>Abs.,</p>
